ac685b43acc81356da8e7a8c6972153e5c179e7b
ltpoole <laborris.poole@gmail.com>
2024-01-11 21:03:58 -0500

    created Product class

---
+++ FullStackAuth_WebAPI/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public float Price { get; set; }
+        [Required]
+        public int StockQuantity { get; set; }
+
+    }
+}
