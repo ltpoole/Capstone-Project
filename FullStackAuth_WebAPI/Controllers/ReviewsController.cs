00557cc845adde8c3fef4fee910d93a0391b9ba6
ltpoole <laborris.poole@gmail.com>
2024-01-22 22:40:26 -0500

    created Reviews Controller

---
+++ FullStackAuth_WebAPI/Controllers/ReviewsController.cs
@@ -0,0 +1,53 @@
+﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using ZstdSharp.Unsafe;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+      
+        // POST api/reviews
+        [HttpPost, Authorize]
+        public IActionResult PostReview([FromBody] Review data)
+        {
+            try
+            {
+                string userId = User.FindFirstValue("id");
+
+                if (string.IsNullOrEmpty(userId))
+                {
+                   return Unauthorized();
+                }
+
+                data.UserId = userId;
+
+                _context.Reviews.Add(data);
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                _context.SaveChanges();
+
+                return StatusCode(201, data);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
