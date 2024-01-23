a2389491b4207a94f98441cc54c6a99db574aa31
ltpoole <laborris.poole@gmail.com>
2024-01-22 23:00:10 -0500

    created ProductDetailsDto class

--- FullStackAuth_WebAPI/DataTransferObjects/ProductDetailsDto.cs
+++ FullStackAuth_WebAPI/DataTransferObjects/ProductDetailsDto.cs
@@ -2,6 +2,8 @@
 {
     public class ProductDetailsDto
     {
-       
+        public List<ReviewWithUserDto> Reviews { get; set; }
+        public double AverageRating { get; set; }
+
     }
 }
--- FullStackAuth_WebAPI/Models/Review.cs
+++ FullStackAuth_WebAPI/Models/Review.cs
@@ -9,11 +9,11 @@ namespace FullStackAuth_WebAPI.Models
         [Key] 
         public int Id { get; set; }
         [Required]
+        public string ProductId { get; set; }
+        [Required]
         public string Text { get; set; }
         [Required]
         public double Rating { get; set; }
-        [Required]
-        public string ProductId { get; set; }
 
 
         [ForeignKey("User")]
