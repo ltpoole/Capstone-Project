b130b7df5e99788ded877d2d9f1df458ef4ae6d8
ltpoole <laborris.poole@gmail.com>
2024-01-27 14:12:55 -0500

    fetched latest YouTube episode on HomePage

--- ReactJWTFrontend_Starter-main/src/pages/HomePage/HomePage.js
+++ ReactJWTFrontend_Starter-main/src/pages/HomePage/HomePage.js
@@ -1,42 +1,44 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import useAuth from "../../hooks/useAuth";
-
 import axios from "axios";
 
 const HomePage = () => {
-  // The "user" value from this Hook contains user information (id, userName, email) from the decoded token
-  // The "token" value is the JWT token sent from the backend that you will send back in the header of any request requiring authentication
-  const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [latestVideo, setLatestVideo] = useState(null);
 
   useEffect(() => {
-    fetchCars();
-  }, [token]);
+    const fetchLatestVideo = async () => {
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/youtube/v3/search?key=AIzaSyBkOOjj0B8q0jiM4G7b6zZhZYoxjFPdigg&channelId=UCj3uS1EabcZU7MAWKC3y3Nw&part=snippet,id&order=date&maxResults=1`
+        );
 
-  const fetchCars = async () => {
-    try {
-      let response = await axios.get("https://localhost:5001/api/cars/myCars", {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      });
-      setCars(response.data);
-    } catch (error) {
-      console.log(error.response.data);
-    }
-  };
+        setLatestVideo(response.data.items[0]);
+      } catch (error) {
+        console.error("Error fetching latest video:", error);
+      }
+    };
+
+    fetchLatestVideo();
+  }, []);
 
   return (
-    <div className="container">
-      {console.log(user)}
-      <h1>Home Page for {user.userName}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
+    <div>
+      <h1>Latest YouTube Episode</h1>
+      {latestVideo && (
+        <div>
+          <h2>{latestVideo.snippet.title}</h2>
+          <p>{latestVideo.snippet.description}</p>
+          <iframe
+            title="Unique Title"
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${latestVideo.id.videoId}`}
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      )}
     </div>
   );
 };
